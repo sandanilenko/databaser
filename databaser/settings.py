6404099fdb2e3016be0df6eded09b9e83c86e7fd
Alexander Danilenko <sandanilenko@gmail.com>
2020-04-28 20:34:15 +0300

    Rename environment variables

--- databaser/settings.py
+++ databaser/settings.py
@@ -9,43 +9,43 @@ from core.helpers import (
 )
 
 # Logger
-LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
+LOG_LEVEL = os.environ.get('DATABASER_LOG_LEVEL', 'INFO')
 logger.setLevel(getattr(logging, LOG_LEVEL, logging.INFO))
 
 # Src database connection params
-SRC_DB_HOST = os.environ.get('SRC_DB_HOST')
-SRC_DB_PORT = os.environ.get('SRC_DB_PORT')
-SRC_DB_SCHEMA = os.environ.get('SRC_DB_SCHEMA', 'public')
-SRC_DB_NAME = os.environ.get('SRC_DB_NAME')
-SRC_DB_USER = os.environ.get('SRC_DB_USER')
-SRC_DB_PASSWORD = os.environ.get('SRC_DB_PASSWORD')
+SRC_DB_HOST = os.environ.get('DATABASER_SRC_DB_HOST')
+SRC_DB_PORT = os.environ.get('DATABASER_SRC_DB_PORT')
+SRC_DB_SCHEMA = os.environ.get('DATABASER_SRC_DB_SCHEMA', 'public')
+SRC_DB_NAME = os.environ.get('DATABASER_SRC_DB_NAME')
+SRC_DB_USER = os.environ.get('DATABASER_SRC_DB_USER')
+SRC_DB_PASSWORD = os.environ.get('DATABASER_SRC_DB_PASSWORD')
 
 # Dst database connection params
-DST_DB_HOST = os.environ.get('DST_DB_HOST')
-DST_DB_PORT = os.environ.get('DST_DB_PORT')
-DST_DB_SCHEMA = os.environ.get('DST_DB_SCHEMA', 'public')
-DST_DB_NAME = os.environ.get('DST_DB_NAME')
-DST_DB_USER = os.environ.get('DST_DB_USER')
-DST_DB_PASSWORD = os.environ.get('DST_DB_PASSWORD')
+DST_DB_HOST = os.environ.get('DATABASER_DST_DB_HOST')
+DST_DB_PORT = os.environ.get('DATABASER_DST_DB_PORT')
+DST_DB_SCHEMA = os.environ.get('DATABASER_DST_DB_SCHEMA', 'public')
+DST_DB_NAME = os.environ.get('DATABASER_DST_DB_NAME')
+DST_DB_USER = os.environ.get('DATABASER_DST_DB_USER')
+DST_DB_PASSWORD = os.environ.get('DATABASER_DST_DB_PASSWORD')
 
 # Test mode parameters
-TEST_MODE = bool(strtobool(os.environ.get('TEST_MODE') or 'False'))
+TEST_MODE = bool(strtobool(os.environ.get('DATABASER_TEST_MODE') or 'False'))
 
 if TEST_MODE:
     logger.warning('TEST MODE ACTIVATED!!!')
 
-KEY_TABLE_NAME = os.environ.get('KEY_TABLE_NAME')
-KEY_COLUMN_NAMES = os.environ.get('KEY_COLUMN_NAMES', '').replace(' ', '').split(',')
-KEY_COLUMN_IDS = os.environ.get('KEY_COLUMN_IDS', '').replace(' ', '').split(',')
+KEY_TABLE_NAME = os.environ.get('DATABASER_KEY_TABLE_NAME')
+KEY_COLUMN_NAMES = os.environ.get('DATABASER_KEY_COLUMN_NAMES', '').replace(' ', '').split(',')
+KEY_COLUMN_IDS = os.environ.get('DATABASER_KEY_COLUMN_IDS', '').replace(' ', '').split(',')
 
-EXCLUDED_TABLES = os.environ.get('EXCLUDED_TABLES', '').split(',')
+EXCLUDED_TABLES = os.environ.get('DATABASER_EXCLUDED_TABLES', '').split(',')
 TABLES_WITH_GENERIC_FOREIGN_KEY = os.environ.get(
-    'TABLES_WITH_GENERIC_FOREIGN_KEY',
+    'DATABASER_TABLES_WITH_GENERIC_FOREIGN_KEY',
     '',
 ).split(',')
 
 TRUNCATE_EXCLUDED_TABLES = os.environ.get(
-    'TRUNCATE_EXCLUDED_TABLES',
+    'DATABASER_TABLES_TRUNCATE_EXCLUDED',
     '',
 ).split(',')
 
